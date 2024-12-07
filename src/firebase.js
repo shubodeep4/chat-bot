54d5c334ef79938fd27c6bf30974b516478fa5de
shubodeep <shubs987@gmail.com>
2024-12-07 16:58:17 +0530

    changes in readme

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,6 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "AIzaSyA4GwP9Fi6AuvDQOganUJsfw1jb9JvB23w",
-  // authDomain: "chat-bot-1b420.firebaseapp.com",
-  // projectId: "chat-bot-1b420",
-  // storageBucket: "chat-bot-1b420.firebasestorage.app",
-  // messagingSenderId: "321011458225",
-  // appId: "1:321011458225:web:5384583fafb91a10f84290",
-  // measurementId: "G-X7VL6ZK3C3"
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
